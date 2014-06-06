6d7df78cf3ceeda01aad3e242aa0e509b8818dce
erikpayerl <erikpayerl@gmail.com>
2014-06-06 13:29:30 +0200

    Code comments

--- FXxy_DSPc5535_code/src/tremolo.c
+++ FXxy_DSPc5535_code/src/tremolo.c
@@ -1,8 +1,17 @@
-// Den här funktionen tar in en sample, depth och LFOspeed. 
-// depth är en skalfaktor som avgör hur djup amplitudmodulationen blir
-// depth skall ligga mellan 0 och 32 som inparameter och motsvarar ett decimaltal mellan 0 och 1
-// LFOspeed avgör hur snabb modulationen blir, högre LFOspeed-> lägre frekvens (OBS)
-// LFOspeed=20 vid 48kHz samplingsfrekvens ger en LFO med frekvens ung. 4.7Hz
+//////////////////////////////////////////////////////////////////////////////
+// * File name:    tremolo.c
+// * 
+// * Function:     tremolo                                                                        
+// * Description:  The input sample is scaled according to a LFO and then output, this generates a tremolo effect. 
+// *               .
+// *                                                                                     
+// * Usage:        Int16  = flanger( Int16 sample,         // input sample
+// *                                 Int16 LFOspeed, // sets the speed for LFO, range(0-254)
+// *                                 Int16 depth, //    decides the maximum scaling, range (0-254)
+// *                                 Int16 opmode,)//  not used.
+// *
+// * Erik Sandgren   2014-06-02                                                           
+//////////////////////////////////////////////////////////////////////////////
 
 #include "tistdtypes.h"
 #include "LFO.h"
@@ -17,12 +26,12 @@ Int16 modsignal;
 Int16 tremolo(Int16 sample, Int16 LFOspeed, Int16 depth, Int16 opmode)
 {	
 	depth = depth>>3;
-	LFOspeed=260-LFOspeed; // Satte 260 för att det inte skall kunna bli för hög frekvens.
-	modulator=LFO(LFOspeed);
-	modsignal=(8192 - depth*256) + depth*modulator;
+	LFOspeed=260-LFOspeed; /*Modifies the parameter to fit the function better */
+	modulator=LFO(LFOspeed); /*Retrieving value of LFO */
+	/* Scaling */
+	modsignal=(8192 - depth*256) + depth*modulator; 
 	output32=(Int32) sample*modsignal;
-	
-	output= output32 >> 13; // stod 14 här innan jag ändrade modsignal
+	output= output32 >> 13; 
 	
 	return output;		
 }
